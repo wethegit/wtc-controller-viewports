0259df1a7276372448b55a25db4f3b96e74654b4
Liam Egan <liam@wethecollective.com>
2019-06-05 10:36:36 -0700

    Adding load handler for automatic trigger

--- src/wtc-controller-viewports.js
+++ src/wtc-controller-viewports.js
@@ -87,7 +87,9 @@ class Viewport extends ElementController {
     this.element.classList.add(`${this.classPrefix}--initialised`);
 
     // This manually sets the initial top offset to provide a single, initial call of the animation event.
-    this.top = this.element.offsetTop - window.scrollY;
+    window.addEventListener('load', () => {
+      this.top = this.element.offsetTop - window.scrollY;
+    });
   }
 
   /**
